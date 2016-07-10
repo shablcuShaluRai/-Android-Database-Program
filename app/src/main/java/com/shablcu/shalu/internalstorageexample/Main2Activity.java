baae59dc7fcb7fb4b5ec7d9c1e4627c242c30459
shalurai <shaluraics22@gmail.com>
2016-07-10 22:36:12 +0900

    get data from file

--- app/src/main/java/com/shablcu/shalu/internalstorageexample/Main2Activity.java
+++ app/src/main/java/com/shablcu/shalu/internalstorageexample/Main2Activity.java
@@ -3,13 +3,18 @@ package com.shablcu.shalu.internalstorageexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main2Activity extends AppCompatActivity {
+    public static final String DEFAULT = "N/A";
     TextView usernameTextview, PasswordTextView;
-    public static final String DEFAULT="N/A";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +28,35 @@ public class Main2Activity extends AppCompatActivity {
     }
 
 
-    public  void load(View view){
+    public void load(View view) {
 
-       
+        try {
+            FileInputStream fileInputStream = openFileInput("Shablcu.txt");
+
+            int read = -1;
+            StringBuffer buffer = new StringBuffer();
+            while ((read = fileInputStream.read()) != -1) {
+                buffer.append((char) read);
+            }
+
+            Log.d("Shablcu",buffer.toString() );
+
+            String txtname=buffer.substring(0,buffer.indexOf(" "));
+            String txtpwd=buffer.substring(buffer.indexOf(" ")+1);
+            usernameTextview.setText(txtname);
+            PasswordTextView.setText(txtpwd);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void previous(View view){
+    public void previous(View view) {
 
-        Toast.makeText(this,"previous",Toast.LENGTH_LONG).show();
-        Intent intent=new Intent(this,MainActivity.class);
+        Toast.makeText(this, "previous", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
